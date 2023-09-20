14e36d3b53f09f4590d229179f87bcc5d29e38a1
David <davtamay@gmail.com>
2023-09-20 01:02:19 -0500

    fixed missing scale property on primitives

--- sync.js
+++ sync.js
@@ -2215,7 +2215,7 @@ module.exports = {
                //modeldata_url = 1, primitive = 2, drawing = 3
             modelType: 2,
             guid: message.guid,
-            latest: {pos: message.pos, rot: message.rot },
+            latest: {pos: message.pos, rot: message.rot, scaleFactor: message.scaleFactor},
             indentifier : message.indentifier,
           
         };
